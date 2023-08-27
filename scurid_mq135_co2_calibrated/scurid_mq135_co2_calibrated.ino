d31258c057f47611982d3f990704948f34d535be
Sushant <sushant.pandey@scurid.com>
2023-08-27 23:49:43 +0200

    updated calibration - WIP

--- scurid_mq135_co2_calibrated/scurid_mq135_co2_calibrated.ino
+++ scurid_mq135_co2_calibrated/scurid_mq135_co2_calibrated.ino
@@ -1,16 +1,23 @@
-#define MQ135_PIN 34 // Analog pin for MQ-135
-float R0 = 1.0; // Replace with your R0 value obtained from calibration
-float S = 0.2; // Replace with your sensitivity factor obtained from calibration
+#define MQ135_PIN 34 // Connect A0 of MQ-135 to GPIO 34
 
 void setup() {
-  Serial.begin(115200);
-  pinMode(MQ135_PIN, INPUT);
+  Serial.begin(115200); // Initialize Serial monitor with 115200 baud rate
+  pinMode(MQ135_PIN, INPUT); // Initialize the MQ135_PIN as an input
 }
 
 void loop() {
-  int sensorValue = analogRead(MQ135_PIN);
-  float concentration = ((sensorValue / R0) - 1) / S; // Simplified formula
-
-  Serial.println("Calibrated Concentration: " + String(concentration));
-  delay(1000);
+  int sensorValue = analogRead(MQ135_PIN); // Read the value from sensor
+  
+  if(sensorValue == 0) {
+    Serial.println("Sensor not connected or not functioning!");
+  } else {
+    Serial.println("Sensor Value: " + String(sensorValue)); // Print the sensor value
+    
+    // Convert the sensor value to concentration (this is a simplified calculation)
+    // Note: For more accurate results, you should calibrate the sensor and possibly use additional hardware
+    float concentration = (sensorValue / 4096.0) * 100.0;
+    Serial.println("Concentration: " + String(concentration) + "%");
+  }
+  
+  delay(1000); // Delay for 1 second
 }
